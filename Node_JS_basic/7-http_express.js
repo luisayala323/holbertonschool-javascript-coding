c11537aa5ccb1d0cd6f1ccba63b52a13430c2d37
Luis <luisayala323@gmail.com>
2023-10-14 01:40:36 -0400

    task 7

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -8,25 +8,18 @@ const app = express();
 module.exports = app;
 
 app.get('/', (req, res) => {
-  // Use status() to set the status code
-  res.status(200).send('Hello Holberton School!');
+  res.setHeader('content-type', 'text/plain');
+  res.send('Hello Holberton School!');
 });
 
-app.get('/students', async (req, res) => {
-  try {
-    // Use status() to set the status code
-    res.status(200).send('This is the list of our students\n' + (await countStudents(DB)).join('\n'));
-  } catch (error) {
-    // Use status() to set the status code and send the error message
-    res.status(500).send(error.message);
-  }
+app.get('/students', (req, res) => {
+  res.setHeader('content-type', 'text/plain');
+  res.write('This is the list of our students\n');
+  countStudents(DB).then((result) => {
+    res.end(result.join('\n'));
+  }).catch((error) => {
+    res.end(`${error.message}`);
+  });
 });
 
-// Use listen() to handle server startup errors
-app.listen(port, (err) => {
-  if (err) {
-    console.error('Error starting server:', err);
-  } else {
-    console.log(`Server is running on port ${port}`);
-  }
-});
+app.listen(port);
