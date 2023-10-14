236384af16ad591699794ab9ca33005eb6aaf012
Luis <luisayala323@gmail.com>
2023-10-14 01:37:35 -0400

    .

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -2,37 +2,24 @@
 const express = require('express');
 const countStudents = require('./3-read_file_async');
 
-const app = express();
+const DB = process.argv[2];
 const port = 1245;
+const app = express();
+module.exports = app;
 
 app.get('/', (req, res) => {
+  res.setHeader('content-type', 'text/plain');
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', async (req, res) => {
-  const databaseFileName = process.argv[2];
-
-  if (!databaseFileName) {
-    console.error('Error: Database file not provided.');
-    res.status(500).send('Internal Server Error: Database file not provided.');
-    return;
-  }
-
-  try {
-    console.log(`Reading database file: ${databaseFileName}`);
-    await countStudents(databaseFileName);
-
-    // The countStudents function already logs the information to the console,
-    // so you can simply send a success response here.
-    res.send('Successfully counted students.');
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-    res.status(500).send(`Internal Server Error: ${error.message}`);
-  }
+app.get('/students', (req, res) => {
+  res.setHeader('content-type', 'text/plain');
+  res.write('This is the list of our students\n');
+  countStudents(DB).then((result) => {
+    res.end(result.join('\n'));
+  }).catch((error) => {
+    res.end(`${error.message}`);
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
-
-module.exports = app;
+app.listen(port);
